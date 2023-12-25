ce78e58352c5c65d396580d4e00c53fe91f4ddb8
Ronny Vedrilla <ronny.vedrilla@ambient.digital>
2023-12-25 15:09:03 +0100

    Year Progression & Taxes

--- apps/city/services/building/maintenance.py
+++ apps/city/services/building/maintenance.py
@@ -4,16 +4,21 @@ from apps.city.models import Savegame
 
 
 class BuildingMaintenanceService:
+    def __init__(self):
+        super().__init__()
+
+        self.savegame, _ = Savegame.objects.get_or_create(id=1)
+
+    def _calculate_taxes(self):
+        return self.savegame.tiles.aggregate(sum_taxes=Sum("building__taxes"))["sum_taxes"]
+
+    def _calculate_maintenance(self):
+        return self.savegame.tiles.aggregate(sum_maintenance=Sum("building__maintenance_costs"))["sum_maintenance"]
+
     def process(self):
         savegame, _ = Savegame.objects.get_or_create(id=1)
 
-        sum_maintenance = savegame.tiles.aggregate(sum_maintenance=Sum("building__maintenance_costs"))[
-            "sum_maintenance"
-        ]
+        savegame.coins += self._calculate_taxes() - self._calculate_maintenance()
+        savegame.current_year += 1
 
-        print(f"Maintenance {sum_maintenance}")
-
-        savegame.coins -= sum_maintenance
         savegame.save()
-
-        return sum_maintenance

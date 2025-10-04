dfb970f332ed5f05545b5775a401904553123283
Ronny Vedrilla <ronny.vedrilla@ambient.digital>
2025-10-04 09:42:57 +0200

    Boa Restrictor Linter

--- apps/city/context_processors/savegame.py
+++ apps/city/context_processors/savegame.py
@@ -1,6 +1,6 @@
 from apps.city.models import Savegame
 
 
-def get_current_savegame(request):
+def get_current_savegame(request) -> dict:
     savegame, _ = Savegame.objects.get_or_create(id=1)
     return {"savegame": savegame}

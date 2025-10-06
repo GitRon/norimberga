70f4cd38cdc1e51f9747faa21184f7d2b8a9ca3c
Ronny Vedrilla <ronny.vedrilla@ambient.digital>
2025-10-06 16:33:33 +0200

    #51: Increase map size

---
+++ apps/city/constants.py
@@ -0,0 +1,7 @@
+"""Constants for the city app."""
+
+# Map size is fixed at 20x20 tiles
+MAP_SIZE = 20
+
+# Initial number of country buildings to place on map generation
+INITIAL_COUNTRY_BUILDINGS = 3

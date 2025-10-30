7b82fe73ac43ae80aa1701a0d7d3722d48a34eb4
Ronny Vedrilla <ronny.vedrilla@ambient.digital>
2025-10-30 09:02:07 +0100

    #65: Terrain images

--- apps/city/admin.py
+++ apps/city/admin.py
@@ -13,7 +13,7 @@ class TileAdmin(admin.ModelAdmin):
 
 @admin.register(Terrain)
 class TerrainAdmin(admin.ModelAdmin):
-    list_display = ("name", "color_class", "probability", "is_water")
+    list_display = ("name", "image_filename", "probability", "is_water")
 
 
 class BuildingInline(admin.TabularInline):
